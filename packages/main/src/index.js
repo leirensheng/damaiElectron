a9bee35535c84621f2aa8058c5724376bb5710e4
leirensheng <leirensheng@163.com>
2023-05-15 12:47:30 +0800

    feat: 自动启动服务器

--- packages/main/src/index.js
+++ packages/main/src/index.js
@@ -1,7 +1,7 @@
 import {app} from 'electron';
 import './security-restrictions';
 import {restoreOrCreateWindow} from '/@/mainWindow';
-import {cmd} from './tool.js';
+import {startServer} from './tool.js';
 /**
  * Prevent electron from running multiple instances.
  */
@@ -37,7 +37,7 @@ app.on('activate', restoreOrCreateWindow);
 app
   .whenReady()
   .then(restoreOrCreateWindow)
-  .then(cmd('cd ../damaiServer && pm2 stop damai && pm2 start damai'))
+  .then(startServer)
   .catch(e => console.error('Failed create window:', e));
 
 /**
